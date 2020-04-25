3c7c3eccc3c770b8e17ce51eaec7e34f550c71d9
Doken Edgar <dokenedgarjr@gmail.com>
2020-04-25 01:07:56 +0100

    test middleware

--- test/middleware/app_middleware_test.dart
+++ test/middleware/app_middleware_test.dart
@@ -2,7 +2,19 @@
 //by 10:41 AM
 //on 23/Oct/2019
 
-/*
+import 'dart:convert';
+
+import 'package:auto_load_api/actions/movies_action.dart';
+import 'package:auto_load_api/data/yts_api.dart';
+import 'package:auto_load_api/middleware/app_middleware.dart';
+import 'package:auto_load_api/models/app_state.dart';
+import 'package:auto_load_api/models/movie.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:redux/redux.dart';
+
+import '../res/res.dart';
+
 void main() {
   tearDown(resetMockitoState);
   test('loadMovies', () async {
@@ -14,13 +26,11 @@ void main() {
     final String data = Res.movieListResult.readAsStringSync();
     final dynamic resultData = jsonDecode(data)['data']['movies'];
 
-    final List<Movie> movieList = List<dynamic>.from(resultData)
-        .map((dynamic it) => Movie.fromJson(it))
-        .toList();
+    final List<Movie> movieList =
+        List<dynamic>.from(resultData).map((dynamic it) => Movie.fromJson(it)).toList();
 
     when(state.pageNumber).thenReturn(pageNumber);
-    when(api.getMovies(captureThat(anything)))
-        .thenAnswer((_) async => movieList);
+    when(api.getMovies(captureThat(anything))).thenAnswer((_) async => movieList);
     when(store.state).thenReturn(state);
     when(store.dispatch(captureThat(anything)));
 
@@ -29,8 +39,7 @@ void main() {
 
     await middleware.loadMovies(store, action, null);
 
-    final VerificationResult verificationResult =
-        verify(store.dispatch(captureThat(anything)));
+    final VerificationResult verificationResult = verify(store.dispatch(captureThat(anything)));
 
     expect(verificationResult.callCount, 2);
     final SetMovies setMovies = verificationResult.captured[0];
@@ -45,4 +54,3 @@ class MockYtsApi extends Mock implements YtsMovieApi {}
 class MockAppState extends Mock implements AppState {}
 
 class MockStore extends Mock implements Store<AppState> {}
-*/
