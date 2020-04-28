459c7c27de4101802ee45aa9b796b75ef55db06f
Doken Edgar <dokenedgarjr@gmail.com>
2020-04-28 12:48:44 +0100

    Change app icon and movie detail app bar color gradient

---
+++ android/app/src/main/java/com/dokenedgar/movieapp/MainActivity.java
@@ -0,0 +1,14 @@
+// package com.example.auto_load_api;
+package com.dokenedgar.movieapp;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
